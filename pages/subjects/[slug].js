58101952efb10fef36e7c9c3a7d2e2f7d03737a6
Israel Sepulveda <sepulveda.israel@gmail.com>
2024-07-02 15:05:48 -0400

    Package Cleanup

--- pages/subjects/[slug].js
+++ pages/subjects/[slug].js
@@ -6,7 +6,6 @@ import Footer from "@/components/Footer"
 import NavBar from "@/components/Navbar"
 import { BsPerson } from "react-icons/bs"
 import { FaWindows, FaApple, FaChrome } from "react-icons/fa"
-import DOMPurify from "isomorphic-dompurify"
 import Image from "next/image"
 import { BiSolidCategoryAlt } from "react-icons/bi"
 
