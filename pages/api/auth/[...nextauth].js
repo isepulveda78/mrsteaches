9bb5e297f7d5f191272e40d1e34791cd05a60fe5
Israel Sepulveda <sepulveda.israel@gmail.com>
2024-07-06 10:14:18 -0400

    Auth Callback Changed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -20,39 +20,6 @@ export default NextAuth({
       if (token?.isAdmin) session.user.isAdmin = token.isAdmin
       return session
     },
-    async signIn({ profile }) {
-        // 1. Connect to database
-        await db()
-        // 2. Check if user exists
-        const userExists = await User.findOne({ email: profile.email })
-        // 3. If not, then add user to database
-        if (!userExists) {
-          // Truncate user name if too long
-          const username = profile.name.slice(0, 20)
-
-          const randomPassword =   Math.random().toString(36).slice(2) + Math.random().toString(36).toUpperCase().slice(2)
-  
-          await User.create({
-            email: profile.email,
-            name: username,
-            password: randomPassword,
-            image: profile.picture,
-          })
-        }
-        // 4. Return true to allow sign in
-        return true
-      },
-      // Modifies the session object
-      async session({ session }) {
-        // 1. Get user from database
-        const user = await User.findOne({ email: session.user.email })
-        // 2. Assign the user id to the session
-        session.user.id = user._id.toString()
-        session.user.name = user.name.toString()
-        session.user.email = user.email.toString()
-        // 3. return session
-        return session
-      },
   },
   providers: [
     CredentialsProvider({
