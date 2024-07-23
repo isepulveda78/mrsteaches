fe91b7288dcff676982ba9e736151ce07b292d13
Israel Sepulveda <sepulveda.israel@gmail.com>
2024-07-22 21:04:59 -0400

    API URL Update

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -20,7 +20,7 @@ const [ password, setPassword ] = useState('')
 const [ image, setImage ] = useState('')
 
 const uploadHandler = async (e) => {
-    const url = `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/upload?upload_preset=${process.env.NEXT_PUBLIC_CLOUDINARY_PRESET}`;
+    const url = `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload?upload_preset=${process.env.NEXT_PUBLIC_CLOUDINARY_PRESET}`;
     try {
       const {
         data: { signature, timestamp },
